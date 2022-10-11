f270bac0b1b40113bca80937cde8f89b84f6a200
spartty <65155903+leehyunk6310@users.noreply.github.com>
2022-10-11 12:12:34 +0900

    [Silver III] Title: 소수 구하기, Time: 16 ms, Memory: 5940 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int arr[1000001]{0, };
+
+int main()
+{
+    int m, n;
+    cin >> m >> n;
+
+    for (int i = 2; i <= n; i++)
+    {
+        arr[i] = i;
+    }
+
+    for (int i = 2; i <= sqrt(n); i++)
+    {
+        if (arr[i] == 0)
+        {
+            continue;
+        }
+        for (int j = i * i; j <= n; j += i)
+        {
+            arr[j] = 0;
+        }
+    }
+
+    for (int i = m; i <= n; i++)
+    {
+        if (arr[i] != 0)
+        {
+            cout << arr[i] << '\n';
+        }
+    }
+    
+}
