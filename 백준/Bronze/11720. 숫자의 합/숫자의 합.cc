bc3c73aa001fbf296518c03b0878191602a21767
spartty <65155903+leehyunk6310@users.noreply.github.com>
2022-12-20 13:49:39 +0900

    [Bronze IV] Title: 숫자의 합, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    int n, sum=0;
+    string s;
+
+    cin >> n >> s;
+    for(int i=0; i<n; i++){
+        sum += int(s[i])-48;
+        
+    }
+    cout << sum;
+}
