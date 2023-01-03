f52303254e0dffaac9fbec7169a1e9a01896d943
spartty <65155903+leehyunk6310@users.noreply.github.com>
2023-01-03 20:47:03 +0900

    [D2] Title: 두 개의 숫자열, Time: 7 ms, Memory: 13,376 KB -BaekjoonHub

--- SWEA/D2/1959. 두 개의 숫자열/두 개의 숫자열.cpp
+++ SWEA/D2/1959. 두 개의 숫자열/두 개의 숫자열.cpp
@@ -1,67 +1,105 @@
-#include <iostream>
-#include <algorithm>
-using namespace std;
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// double b;
+// char g;
+// String var;
+// long AB;
+// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
+// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
+// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
+// var = sc.next();                            // 문자열 1개 입력받는 예제
+// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;                            
+// double b = 1.0;               
+// char g = 'b';
+// String var = "ABCDEFG";
+// long AB = 12345678901234567L;
+//System.out.println(a);                       // int 변수 1개 출력하는 예제
+//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
+//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
+//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
+//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+import java.util.Scanner;
+import java.io.FileInputStream;
 
-int main()
+/*
+   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
+   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
+ */
+class Solution
 {
-	int test_case;
-	int T;
-	cin >> T;
-	int A[20];
-	int B[20];
-
-	for (test_case = 1; test_case <= T; ++test_case)
+	public static void main(String args[]) throws Exception
 	{
-		int N, M;
-		int maxVal = -1;
-		cin >> N >> M;
+		/*
+		   아래의 메소드 호출은 앞으로 표준 입력(키보드) 대신 input.txt 파일로부터 읽어오겠다는 의미의 코드입니다.
+		   여러분이 작성한 코드를 테스트 할 때, 편의를 위해서 input.txt에 입력을 저장한 후,
+		   이 코드를 프로그램의 처음 부분에 추가하면 이후 입력을 수행할 때 표준 입력 대신 파일로부터 입력을 받아올 수 있습니다.
+		   따라서 테스트를 수행할 때에는 아래 주석을 지우고 이 메소드를 사용하셔도 좋습니다.
+		   단, 채점을 위해 코드를 제출하실 때에는 반드시 이 메소드를 지우거나 주석 처리 하셔야 합니다.
+		 */
+		//System.setIn(new FileInputStream("res/input.txt"));
 
-		for (int i = 0; i < N; i++)
-			cin >> A[i];
-		for (int i = 0; i < M; i++)
-			cin >> B[i];
+		/*
+		   표준입력 System.in 으로부터 스캐너를 만들어 데이터를 읽어옵니다.
+		 */
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+		/*
+		   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+		*/
 
-		// 길이가 짧은 녀석이 움직이면서 계산해야한다.
-		if (N > M)
+		for(int test_case = 1; test_case <= T; test_case++)
 		{
-			int indexA = 0;
-			while (indexA != N - M)
-			{
-				int sum = 0;
-				for (int i = 0; i < M; i++)
-				{
-					sum += B[i] * A[indexA + i];
-				}
-				indexA++;
-				maxVal = max(maxVal, sum);
-			}			
-		}
-		else if (N < M)
-		{
-			int indexB = 0;
-			while (indexB != M - N + 1)
-			{
-				int sum = 0;
-				for (int i = 0; i < N; i++)
-				{
-					sum += B[indexB + i] * A[i];
-				}				
-				indexB++;
-				maxVal = max(maxVal, sum);
-			}
-		}
-		else
-		{
+		int N = sc.nextInt();
+			int M = sc.nextInt();
+			int arr1[] = new int[N];
+			int arr2[] = new int[M];
+			int diff;
 			int sum = 0;
-			for (int i = 0; i < N; i++)
-			{
-				sum += B[i] * A[i];
+			int answer = -1;
+			
+			for (int i=0; i<N; i++) {
+				arr1[i] = sc.nextInt();
 			}
-			maxVal = max(maxVal, sum);
+			for (int i=0; i<M; i++) {
+				arr2[i] = sc.nextInt();
+			}
+			
+			// 길이가 짧은 것을 이동시켜야 한다.
+			if (N > M) {				
+				diff = N - M;
+				for (int i=0; i<=diff; i++) {
+					for (int j=0; j<M; j++) {
+						sum += arr1[j+i] * arr2[j];
+					}
+					answer = Math.max(answer, sum);					
+					sum = 0;					
+				}
+			}
+			else if (N < M) {
+				diff = M - N;
+				for (int i=0; i<=diff; i++) {
+					for (int j=0; j<N; j++) {
+						sum += arr1[j] * arr2[j+i];
+					}
+					answer = Math.max(answer, sum);					
+					sum = 0;					
+				}
+			}
+			else {
+				for (int i=0; i<N; i++) {
+					sum += arr1[i] * arr2[i];
+				}
+				answer = Math.max(answer, sum);
+			}
+			System.out.println("#" + test_case + " " + answer);
 		}
-
-		cout << "#" << test_case << " " << maxVal << '\n';
 	}
-
-	return 0;
 }
